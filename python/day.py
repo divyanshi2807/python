aeecde1a1b715346581887c60069e589f72fa501
divyanshi2807 <anshiprakash901@gmail.com>
2025-08-26 10:50:44 +0530

    Update day.py

--- python/day.py
+++ python/day.py
@@ -1,3 +1,15 @@
-day_num = int(input("Enter a number (1-7): "))
-days = {1: "Sunday", 2: "Monday", 3: "Tuesday", 4: "Wednesday", 5: "Thursday", 6: "Friday", 7: "Saturday"}
-print(days.get(day_num, "Invalid input"))
+day=int(input("enter the number ofday:"))
+if day==1:
+    print("it's monday")
+elif day==2:
+    print("it's tuesday")
+elif day==3:
+    print("it's wednesday")
+elif day==4:
+    print("it's thursday")
+elif day==5:
+    print("it's friday")
+elif day==6:
+    print("it's saturday")
+else:
+    print("it's sunday")
