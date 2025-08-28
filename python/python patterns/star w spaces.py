7e6735ef522b8e22b4c7e060e2bacfe3fdb58a4d
divyanshi2807 <anshiprakash901@gmail.com>
2025-08-29 00:07:37 +0530

    Add files via upload

---
+++ python/python patterns/star w spaces.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number of rows: "))
+b = int(input("Enter number of columns: "))
+for i in range(a):
+    if b > 1:
+        print("* " + "_ " * (b - 2) + "*")
+    else:
+        print("*")
